11bb44cf53a18ef82705d7c56788f4bdef948725
GuYun-D <1946306647@qq.com>
2021-04-01 14:26:42 +0800

    访问state数据的第二种方式

--- 8 - vuex/vuex_demo01/src/components/Addition.vue
+++ 8 - vuex/vuex_demo01/src/components/Addition.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
-      <!-- 在vue单文件中可以省略this -->
+    <!-- 在vue单文件中可以省略this -->
     <!-- <h3>当前最新的count值为：{{this.$store.state.count}}</h3> -->
-    <h3>当前最新的count值为：{{$store.state.count}}</h3>
+    <!-- 访问state数据的第一种方式 -->
+    <h3>当前最新的count值为：{{ $store.state.count }}</h3>
     <button>+1</button>
   </div>
 </template>
 
 <script>
-export default {};
+export default {}
 </script>
 
 <style>
--- 8 - vuex/vuex_demo01/src/components/Subtraction.vue
+++ 8 - vuex/vuex_demo01/src/components/Subtraction.vue
@@ -1,12 +1,29 @@
 <template>
   <div>
-    <h3>当前最新的count值为：</h3>
+    <h3>当前最新的count值为：{{count}}</h3>
     <button>-1</button>
+    <!-- 
+        访问state数据的第二种方式 
+        从vuex中按需导入mapState函数
+        通过刚才的函数，将当前组件所需要的全局数据映射为当前组建的计算属性
+    -->
   </div>
 </template>
 
 <script>
-export default {};
+import { mapState } from 'vuex'
+export default {
+    data(){
+        return {
+
+        }
+    },
+
+
+    computed: {
+        ...mapState(["count"])
+    }
+}
 </script>
 
 <style>
