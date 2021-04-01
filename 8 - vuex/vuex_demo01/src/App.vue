e9fafadaf56b7dcecd4e16dbf0dfa21409eade00
GuYun-D <1946306647@qq.com>
2021-04-01 14:13:48 +0800

    初始化计数器

--- 8 - vuex/vuex_demo01/src/App.vue
+++ 8 - vuex/vuex_demo01/src/App.vue
@@ -1,31 +1,27 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div>
+    <my-addition></my-addition>
+    <my-subtraction></my-subtraction>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+<script>
+import Addition from './components/Addition.vue'
+import Subtraction from './components/Subtraction.vue'
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+  // 注册私有组件
+  components: {
+    'my-addition': Addition,
+    'my-subtraction': Subtraction,
+  },
 }
+</script>
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style scoped>
 </style>
