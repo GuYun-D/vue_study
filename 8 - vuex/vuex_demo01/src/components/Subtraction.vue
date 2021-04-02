ccabffc0624b5f185b14e1b5252ab8d255d6f78b
GuYun-D <1946306647@qq.com>
2021-04-02 15:55:52 +0800

    getters的第二种访问方式

--- 8 - vuex/vuex_demo01/src/components/Subtraction.vue
+++ 8 - vuex/vuex_demo01/src/components/Subtraction.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h3>当前最新的count值为：{{ count }}</h3>
+    <h3>当前最新的count值为：{{ showNum }}</h3>
     <!-- <button @click="btnHandler1">-1</button>
     <button @click="btnHandler2">-5</button>
     <button @click="btnHandler3">-1 async</button> -->
@@ -17,7 +18,7 @@
 </template>
 
 <script>
-import { mapState, mapMutations, mapActions } from 'vuex'
+import { mapState, mapMutations, mapActions, mapGetters } from 'vuex'
 export default {
   data() {
     return {}
@@ -45,6 +46,7 @@ export default {
 
   computed: {
     ...mapState(['count']),
+    ...mapGetters(['showNum']),
   },
 }
 </script>
